932a44a9e5fa78781cb2f0f0520af2bf6f638dcf
ofadiman <ofadiman@gmail.com>
2021-10-16 16:09:06 +0200

    chore: remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,12 @@ import { Module, ValidationPipe } from '@nestjs/common'
 import { APP_PIPE } from '@nestjs/core'
 
 import { AccountsModule } from './accounts/accounts.module'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { ConfigurationModule } from './configuration/configuration.module'
 import { DatabaseModule } from './database/database.module'
 
 @Module({
-  controllers: [AppController],
   imports: [DatabaseModule, ConfigurationModule, AccountsModule],
   providers: [
-    AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
